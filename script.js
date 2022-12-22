4b2e82985ed64acd1bdfc69ccbf36cdb75f48bbf
Paulo Sergio <pauloserg861@gmail.com>
2022-12-22 14:24:34 -0300

    conectando axios

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 import "regenerator-runtime/runtime";
 import axios from "axios";
+
+const BASE_URL = `http://localhost:3004/api`;
+
+const axiosRequest = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/upload`);
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const main = async () => {
+  await axiosRequest();
+};
+
+main();
