41433c4b3211dc86113bc1431e1c1e277274adbb
Paulo Sergio <pauloserg861@gmail.com>
2022-12-22 14:53:40 -0300

    incluindo criação de elementos (teste)

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ const BASE_URL = `http://localhost:3004/api`;
 const axiosRequest = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/upload`);
-    console.log(response.data);
+    console.log(response.data.uploadList);
   } catch (error) {
     console.log(error);
   }
 };
+
+/*const createImageElement = (image) => {
+  const img = document.createElement("img");
+
+  img.setAttribute("src", this.response.data.uploadList.imagePath);
+  img.src = `${this.response.data.uploadList.imagePath}`;
+
+  console.log(imageElement);
+};*/
 
 const main = async () => {
   await axiosRequest();
